686237d7a569a572b16a77df0fb2c0bf19510a3c
Andrew Marchukov <metla40@gmail.com>
2019-12-26 22:30:13 +0300

    fixed added

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,73 +7,73 @@ for (let nav of navs) {
 		nav.classList.toggle('active');
 	}
 }
-class Modal {
-	constructor(openBtns, closeBtns, animation) {
-		this.openBtns = openBtns;
-		this.closeBtns = closeBtns;
-		if (animation) {
-			this.openAnimate = animation.openAnimate;
-			this.closeAnimate = animation.closeAnimate;
-		}
+// class Modal {
+// 	constructor(openBtns, closeBtns, animation) {
+// 		this.openBtns = openBtns;
+// 		this.closeBtns = closeBtns;
+// 		if (animation) {
+// 			this.openAnimate = animation.openAnimate;
+// 			this.closeAnimate = animation.closeAnimate;
+// 		}
 
-		for (let btn of this.openBtns) this.openModal(btn);
-		for (let btn of this.closeBtns) this.closeModal(btn);
-	}
+// 		for (let btn of this.openBtns) this.openModal(btn);
+// 		for (let btn of this.closeBtns) this.closeModal(btn);
+// 	}
 
-	openModal(btn) {
-		btn.addEventListener('click', () => {
-			const attr = this.currAttr(btn);
-			const modal = document.querySelector(`#${attr}`);
-			this.openAnimate(modal);
-		});
-	};
+// 	openModal(btn) {
+// 		btn.addEventListener('click', () => {
+// 			const attr = this.currAttr(btn);
+// 			const modal = document.querySelector(`#${attr}`);
+// 			this.openAnimate(modal);
+// 		});
+// 	};
 
-	closeModal(btn) {
-		btn.addEventListener('click', () => {
-			const attr = this.currAttr(btn);
-			const modal = document.querySelector(`#${attr}`);
-			this.closeAnimate(modal);
-		});
-	};
+// 	closeModal(btn) {
+// 		btn.addEventListener('click', () => {
+// 			const attr = this.currAttr(btn);
+// 			const modal = document.querySelector(`#${attr}`);
+// 			this.closeAnimate(modal);
+// 		});
+// 	};
 
-	currAttr(btn) {
-		return btn.hasAttribute('data-open') ? btn.getAttribute('data-open') : btn.getAttribute('data-close');
-	}
-}
+// 	currAttr(btn) {
+// 		return btn.hasAttribute('data-open') ? btn.getAttribute('data-open') : btn.getAttribute('data-close');
+// 	}
+// }
 
-const openBtns = document.querySelectorAll('.modal__open');
-const closeBtns = document.querySelectorAll('.modal__close');
+// const openBtns = document.querySelectorAll('.modal__open');
+// const closeBtns = document.querySelectorAll('.modal__close');
 
-const modalClass = new Modal(openBtns, closeBtns, {
+// const modalClass = new Modal(openBtns, closeBtns, {
 
-	openAnimate(modal) {
-		modal.classList.add('active');
-		document.body.classList.add('no-scroll');
-		setTimeout(() => {
-			modal.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-			modal.querySelector('.modal__dialog').style.transform = "rotateX(0)";
-		}, 200);
-	},
+// 	openAnimate(modal) {
+// 		modal.classList.add('active');
+// 		document.body.classList.add('no-scroll');
+// 		setTimeout(() => {
+// 			modal.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+// 			modal.querySelector('.modal__dialog').style.transform = "translateY(0)";
+// 		}, 200);
+// 	},
 
-	closeAnimate(modal) {`			`
-		modal.querySelector('.modal__dialog').style.transform = "rotateX(90deg)";
-		modal.style.backgroundColor = null;
-		setTimeout(() => {
-			modal.classList.remove('active');
-			document.body.classList.remove('no-scroll');
-		}, 200);
-	}
-});
+// 	closeAnimate(modal) {`			`
+// 		modal.querySelector('.modal__dialog').style.transform = "translateY(-100%)";
+// 		modal.style.backgroundColor = null;
+// 		setTimeout(() => {
+// 			modal.classList.remove('active');
+// 			document.body.classList.remove('no-scroll');
+// 		}, 200);
+// 	}
+// });
 
-window.addEventListener('click', function (event) {
-	const modals = document.querySelectorAll('.modal');
+// window.addEventListener('click', function (event) {
+// 	const modals = document.querySelectorAll('.modal');
 
-	for (let modal of modals) {
-		if (event.target == modal) {
-			modalClass.closeAnimate(modal);
-		}
-	}
-});
+// 	for (let modal of modals) {
+// 		if (event.target == modal) {
+// 			modalClass.closeAnimate(modal);
+// 		}
+// 	}
+// });
 
 $(document).ready(function(){
 	$('#slider').slick({
